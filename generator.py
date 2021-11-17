2c016bf9a5d7c0a719fa4ef62e9ea3f0d849c111
Bryan`Y. Limz <superuser@brylimz.com>
2021-11-17 15:11:31 +0700

    finish

---
+++ generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city = input("Which city did you grow up in")
+pet = input("What is the name of a pet?")
+print("Your band name could be " + city + " " + pet)
